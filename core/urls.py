cc5d079892aa135bf388b95c82e0491175beeaf7
nicky <nicksonlangat95@gmail.com>
2020-07-13 18:15:35 +0300

    fixes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, HotelList, AttractionList, HotelDetail, AttractionDetail 
 urlpatterns = [
-    path('hotels',views.hotels, name="hotels"),
-    path('places',views.places, name="places"),
+    path('', HomePageView.as_view(), name='home'),
+    path('hotels', HotelList.as_view(), name='hotels'),
+    path('attractions', AttractionList.as_view(), name='attractions'),
+    path('hotel/<int:pk>/', HotelDetail.as_view(), name='hotel_detail'),
+    path('attraction/<int:pk>/', AttractionDetail.as_view(), name='attraction_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView 
 from .models import *
 # Create your views here.
 
-def index(request):
-	return render(request, "home.html")
-
-def hotels(request):
-	hotels=Hotel.objects.all()
-	return render(request, "index.html",{"hotels":hotels})
-
-def places(request):
-	places=Attraction.objects.all()
-	return render(request, "places.html",{"places":places})
+class HomePageView(TemplateView):
+	template_name ="home.html"
+	
+class AttractionList(ListView):
+	model=Attraction
+	template_name="attractions.html"
+	
+class HotelList(ListView):
+	model=Hotel 
+	template_name="hotels.html"
+	
+class HotelDetail(DetailView):
+	model=Hotel 
+	template_name="hotel.html"
+	
+class AttractionDetail(DetailView):
+	model=Attraction
+	template_name="attraction.html"
 	
